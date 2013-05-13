aba63b583672378941e3457c5968a996199f8acf
Colin Wren <colinwrenca@gmail.com>
2013-05-13 15:11:34 -0700

    Add tests for $.prepareText

--- test/dissolve_test.js
+++ test/dissolve_test.js
@@ -15,14 +15,11 @@
     }
   });
 
-  test('is chainable', 1, function() {
-    strictEqual(this.elems.prepareText(), this.elems, 'should be chainable');
-  });
-
   test('prepares target element correctly', 1, function() {
+    console.log($);
     var text = this.elems.filter('#text');
     var charLength = text.html().length;
-    text.prepareText();
+    text.html($.prepareText(text.html()));
     var spanCount = text.find('span').length;
     equal(spanCount, charLength);
   });
@@ -33,7 +30,45 @@
     // Subtract length of span tags
     charLength = charLength - 13;
     var startSpanCount = textTag.find('span').length;
-    textTag.prepareText();
+    textTag.html($.prepareText(textTag.html()));
+    var spanCount = textTag.find('span').length;
+    equal(spanCount, charLength + startSpanCount );
+  });
+
+  module('jQuery.prepareElement', {
+    // This will run before each test in this module.
+    setup: function() {
+      this.elems = $('#qunit-fixture').children();
+      $('#text, #text-with-tags')
+        .clone()
+        .appendTo('#temp');
+      this.elems = $('#temp').children();
+    },
+
+    teardown: function() {
+     $('#temp').empty();
+    }
+  });
+
+  test('is chainable', 1, function() {
+    strictEqual(this.elems.prepareElement(), this.elems, 'should be chainable');
+  });
+
+  test('prepares target element correctly', 1, function() {
+    var text = this.elems.filter('#text');
+    var charLength = text.html().length;
+    text.prepareElement();
+    var spanCount = text.find('span').length;
+    equal(spanCount, charLength);
+  });
+
+  test('prepares target element that contains tags correctly', 1, function() {
+    var textTag = this.elems.filter('#text-with-tags');
+    var charLength = textTag.html().length;
+    // Subtract length of span tags
+    charLength = charLength - 13;
+    var startSpanCount = textTag.find('span').length;
+    textTag.prepareElement();
     var spanCount = textTag.find('span').length;
     equal(spanCount, charLength + startSpanCount );
   });
@@ -59,7 +94,7 @@
 
   asyncTest('calls callback with correct context', 1, function() {
     var text = this.elems.filter('#text');
-    text.prepareText();
+    text.prepareElement();
     text.fadeCharacters({opacity: 0.2, fadeTime: 1, fadeOffset: 1}, function() {
       equal(this.attr('id'), text.attr('id'));
       start();
@@ -68,7 +103,7 @@
 
   asyncTest('fades elements to specified opacity', 1, function() {
     var text = this.elems.filter('#text');
-    text.prepareText();
+    text.prepareElement();
     text.fadeCharacters({opacity: 0.2, fadeTime: 1, fadeOffset: 1}, function() {
       var elementOpacity = text.children().first().css('opacity') * 10;
       equal(Math.round(elementOpacity), 2);
